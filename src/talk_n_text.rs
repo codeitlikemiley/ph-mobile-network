f3b6a002f50f4c197b5c9f0bd270e863f7cb8f8c
Uriah <codeitlikemiley@gmail.com>
2024-04-28 12:56:55 +0800

    added err unrecognized_prefix

--- src/talk_n_text.rs
+++ src/talk_n_text.rs
@@ -18,9 +18,66 @@ impl Validate for TNT {
         if !number.chars().all(char::is_numeric) {
             return Err(MobileNetworkError::NonNumeric);
         }
+
         if number.len() != 11 {
             return Err(MobileNetworkError::InvalidLength);
         }
-        Ok(self.0.is_match(number))
+
+        if !self.0.is_match(number) {
+            return Err(MobileNetworkError::UnrecognizedPrefix(number.to_owned()));
+        }
+
+        Ok(true)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn is_ok_pattern_initialization() {
+        let prefixes = ["0917", "0925"];
+        let result = TNT::new(&prefixes);
+        // no error on initialization
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn is_valid() {
+        let prefixes = ["0917", "0925"];
+        let result = TNT::new(&prefixes).unwrap();
+        // exact length and prefix  and is a valid number
+        assert!(result.validate("09171234567").unwrap());
+    }
+
+    #[test]
+    fn is_err_for_non_numeric() {
+        let prefixes = ["0917", "0925"];
+        let result = TNT::new(&prefixes).unwrap();
+        // contains letters
+        assert!(result.validate("0917abc4567").is_err());
+    }
+
+    #[test]
+    fn is_err_on_invalid_length() {
+        let prefixes = ["0917", "0925"];
+        let result = TNT::new(&prefixes).unwrap();
+        // 9 digits
+        assert!(result.validate("0917123456").is_err());
+        // 12 digits
+        assert!(result.validate("091712345678").is_err());
+    }
+
+    #[test]
+    fn is_err_on_unrecognized_prefix() {
+        let prefixes = ["0917", "0925"];
+
+        let result = TNT::new(&prefixes).unwrap();
+
+        let result = result.validate("09991234567");
+
+        assert!(result.is_err());
     }
 }
